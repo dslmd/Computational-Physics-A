7d2f6fcfc928b8e49a5985c7e4e35fc93804e7fc
Mint <54655274+dslmd@users.noreply.github.com>
2020-11-28 21:42:12 +0800

    update hw13/14

--- 13/第十三题代码.c
+++ 13/第十三题代码.c
@@ -51,53 +51,51 @@ int Seed()	//ͨ搞出来一个种子值
 }
 
 
-void Metropolis_Hasting_sampling(int seed,float alpha, float beta, float gamma) //进行Metropolis Hasting抽样，保存抽样结果到Metropolis_Hasting_sampling.txt，并且把最后的积分结果printf到屏幕上
+void Metropolis_Hasting_sampling(int seed,float alpha, float beta, float gamma) //进行Metropolis Hasting抽样，保存抽样结果到Results.txt，并且把最后的积分结果printf到屏幕上
 {
     double M = M_16807;//参数
 	//迭代量为z
 	int z = seed;//定义种子值
-	double rand_number = ((float)z / M);//定义随机数
+	double rand_number = 0;//定义随机数
     float x_prime = 0;//定义探索值
     int i = 0;//定义循环变量i
     float r = 0; //定义用来和随机变量比较的值
 
     //定义N次抽样点结果
     int N = 100000;
-    float x[100000]={0};
-    x[0]=1;
-//    printf("This first term x0 is exactly %f",x[0]); //验证x0有没有被初始化为1
+    int right = 0;//为了计算抽样效率
+    float total = 0;//定义x的求和
+    float temp = 1; //定义每一次的迭代变量，初始值为1
+    float efficient = 0;
 
 	FILE* fp = NULL; //文件指针
-    fp = fopen("Metropolis_Hasting_sampling.txt", "a"); //打开文件
-    fprintf(fp, "alpha=%f,beta=%f,gamma=%f\n", alpha,beta,gamma); //写入文件
+    fp = fopen("Results.txt", "a"); //打开文件
+
     
-    for ( i = 0; i < N-1; i++)
+    for ( i = 0; i < N; i++)
     {
         z = Schrage_int(z);
         rand_number = ((float)z / M);
         x_prime = (-1) *gamma * log(rand_number);
-        r = pow((x_prime/x[i]),(alpha-1))*exp((x[i]-x_prime)/beta)*exp((x_prime-x[i])/gamma);
+        r = pow((x_prime/temp),(alpha-1))*exp((temp-x_prime)/beta)*exp((x_prime-temp)/gamma);
+//        printf("The x_prime is %f\n",x_prime);
         if (rand_number < min(1,r))
         {
-            x[i+1]=x_prime;
+            temp = x_prime;
+            right += 1;
         }
         else
         {
-            x[i+1]=x[i];
+            temp = temp;
         }
+        total += temp;
+//        printf("%f\n",temp);
     }
-
+//    printf("Total is %f",total);
     //下面的代码干两件事：1.把数据写入txt文件；2.计算积分
-    float integral = 0;
-    
-    for ( i = 0; i < N; i++)
-    {
-        fprintf(fp, "%f\n", x[i]); //写入文件
-        integral += (x[i]-alpha*beta)*(x[i]-alpha*beta);
-    }
-
-    integral = integral / N;
-    printf("The integral is %f\n",integral);
+    total = total / N;
+    efficient = ((float)right/N);
+    fprintf(fp, "%f %f %f\n",gamma,total,efficient); //写入文件
 
 	fclose(fp);		//关闭打开的文件
     
@@ -105,9 +103,9 @@ void Metropolis_Hasting_sampling(int seed,float alpha, float beta, float gamma) 
 
 void main()
 {
-    float alpha = 2, beta = 4;
-    float gamma = 1;
-    for ( gamma = 1; gamma <= 7; gamma=gamma+1)
+    float alpha = 2, beta = 1;
+    float gamma = 0.1;
+    for ( gamma = 0.1; gamma <= 20; gamma=gamma+0.1)
     {
         Metropolis_Hasting_sampling(Seed(),alpha,beta,gamma); 
     }
